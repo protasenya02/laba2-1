1a47f408ce104546895e9177a255188839976caa
Valentina Protasenya <55259005+protasenya02@users.noreply.github.com>
2020-08-04 12:33:42 +0300

    Add files via upload

---
+++ laba2part1.dpr
@@ -0,0 +1,38 @@
+program laba2part1;
+
+{$APPTYPE CONSOLE}
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+var
+  x, f, s: real;
+  k: integer;
+
+begin
+  x := 0.6;
+  f := 0;
+  while x < (1.125) do
+
+  begin
+    s := 0;
+    k := 1;
+    for k := 1 to 15 do
+    begin
+      s := s + (Cos(sqrt(k * x) / (exp(ln(sqr(k) - 1 / 3) * (1 / 3)))));
+
+      if k >= 10 then
+
+      begin
+        f := s + (k * exp(x)) + (exp(ln(k) * (x / 2)));
+        writeln('n=', k:2, '     x=', x:4:2, '     f=', f:6:2);
+      end;
+    end;
+
+    x := x + 0.25;
+  end;
+
+  readln
+
+end.
